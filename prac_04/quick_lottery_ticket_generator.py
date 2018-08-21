60efbb52a65a1affdd4ba6ea81515544bd4d50c9
Samuel Hayes <sam.hayes1999@gmail.com>
2018-08-21 15:28:23 +1000

    Add comments to quickpick

--- prac_04/quick_lottery_ticket_generator.py
+++ prac_04/quick_lottery_ticket_generator.py
@@ -1,8 +1,8 @@
 import random
 ticket = [0, 0, 0, 0, 0, 0]
 purchase = int(input("How many quick picks? "))
-for i in range(purchase):
-    for j in range(0, 6, 1):
+for i in range(purchase):       #loops for amount of tickets
+    for j in range(0, 6, 1):    #generates 6 random numbers for a ticket
         ticket[j] = int(random.randint(1, 45))
-    ticket.sort()
-    print(" ".join("{:2}".format(purchase) for purchase in ticket))
+    ticket.sort()   #orders numbers
+    print(" ".join("{:2}".format(purchase) for purchase in ticket)) #prints as string
