ba53e39199aa66e7edf71c8f28b36a7ae28d2a4e
Samuel Hayes <sam.hayes1999@gmail.com>
2018-08-21 14:32:17 +1000

    Add numbers_averager

---
+++ prac_04/number_averager.py
@@ -0,0 +1,10 @@
+numbers = []
+while len(numbers) < 5:
+    number = int(input("Number: "))
+    numbers.append(number)
+print("The first number is {}".format(numbers[0]))
+print("The last number is {}".format(numbers[-1]))
+print("The smallest number is {}".format(min(numbers)))
+print("The largest number is {}".format(max(numbers)))
+print("The average of the numbers is {}".format(sum(numbers) / len(numbers)))
+
