89a5ffcfb74af51dc7ffc6786760d373a7b4bbb9
jc462895 <sam.hayes1999@gmail.com>
2018-09-11 15:05:44 +1000

    Add some suggestions for Lindsay

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,9 +1,15 @@
 from prac_06.guitar import Guitar
 
 Gibson = Guitar("Gibson", 1922, 1500)
+Yamaha = Guitar("Yamaha", 1999, 2000)
 print(Gibson)
 print(Gibson.get_age())
 print(Gibson.is_vintage())
 
 print("{} get_age() - Expected {}. Got {}".format(Gibson.name, 96, Gibson.get_age()))
 print("{} is_vintage() - Expected {}. Got {}".format(Gibson.name, "True", Gibson.is_vintage()))
+print("{} get_age() - Expected {}. Got {}".format(Yamaha.name, 19, Yamaha.get_age()))
+print("{} is_vintage() - Expected {}. Got {}".format(Yamaha.name, "False", Yamaha.is_vintage()))
+
+
+# add another test
--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -3,7 +3,6 @@ from prac_06.guitar import Guitar
 print("My guitars!")
 guitars = []
 name = input("Name: ")
-
 while name != "":
     year = int(input("Year: "))
     value = int(input("Cost $: "))
@@ -19,7 +18,7 @@ for i, guitar in enumerate(guitars):
         vintage_string = " (vintage)"
     else:
         vintage_string = ""
-    print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
+    print("Guitar {}: {:<20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
                                                               vintage_string))
 # print(guitars)
 # print(Guitar)
