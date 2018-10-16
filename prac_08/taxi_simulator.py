39256cd9ccc743a58b388bda4e1a822a5a7e8831
jc462895 <sam.hayes1999@gmail.com>
2018-10-16 15:30:44 +1000

    Begin cleanup_files. Fix taxi_simulator

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -5,18 +5,24 @@ taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTax
 current_taxi = None
 total_fare = 0
 print("Lets drive:\n q)uit, c)hoose taxi, d)rive")
-choice = input('>>>')
-while choice != 'q'.upper():
-    if choice == 'c'.upper():
+choice = str(input('>>>')).upper()
+
+
+def show_taxis():
+    i = 0
+    for taxi in taxis:
+        print("{}. {}".format(i, (taxis[i])))
+        i += 1
+
+
+while choice != 'Q':
+    if choice == 'C':
         print("Taxis available:")
-        i = 0
-        for taxi in taxis:
-            print("{}. {}".format(i, (taxis[i])))
-            i += 1
+        show_taxis()
         taxi_choice = int(input("Choose taxi: "))
         current_taxi = taxis[taxi_choice]
         print("Your taxi is: {}".format(current_taxi))
-    elif choice == 'd'.upper():
+    elif choice == 'D':
         distance = int(input("Distance: "))
         current_taxi.drive(distance)
         print("Your {} trip will cost you {}".format(current_taxi.name, current_taxi.get_fare()))
@@ -25,5 +31,6 @@ while choice != 'q'.upper():
     else:
         print("Please enter valid input")
     print("q)uit, c)hoose taxi, d)rive")
-    choice = input('>>>')
+    choice = str(input('>>>')).upper()
+show_taxis()
 print("Your total taxi fare is: ${}".format(total_fare))
---
+++ prac_09/cleanup_files.py
@@ -0,0 +1,65 @@
+"""
+CP1404/CP5632 Practical
+Demos of various os module examples
+"""
+import shutil
+import os
+
+
+def main():
+    """Demo os module functions."""
+    print("Starting directory is: {}".format(os.getcwd()))
+
+    # Change to desired directory
+    os.chdir('Lyrics/Christmas')
+
+    # Print a list of all files in current directory
+    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
+
+    # Make a new directory
+    try:
+        os.mkdir('temp')
+    except FileExistsError:
+        pass
+
+    # Loop through each file in the (current) directory
+    for filename in os.listdir('.'):
+        # Ignore directories, just process files
+        if os.path.isdir(filename):
+            continue
+
+        new_name = get_fixed_filename(filename)
+        print("Renaming {} to {}".format(filename, new_name))
+
+        shutil.move(filename, 'temp/' + new_name)
+
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    return new_name
+
+
+def demo_walk():
+    """Process all subdirectories using os.walk()."""
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        # print("Directory:", directory_name)
+        # print("\tcontains subdirectories:", subdirectories)
+        # print("\tand files:", filenames)
+        # print("(Current working directory is: {})".format(os.getcwd()))
+
+        for filename in filenames:
+            for i, letter in enumerate(filename):
+                letter[0].isupper()
+                if letter[i-1].islower():
+                    if letter[i].isupper():
+                        letter[i+1] = '_'
+            print(filename)
+            # current_file = os.path.join(directory_name, filename)
+            # new_name = get_fixed_filename(filename)
+            # updated_file = os.path.join(directory_name, new_name)
+            # os.rename(current_file, updated_file)
+
+# main()
+demo_walk()
