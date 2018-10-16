58a9399dda258bf80d36e6b5dd28667b99fef453
Samuel-Hayes <sam.hayes1999@gmail.com>
2018-10-16 10:15:44 +1000

    Finish prac 8 taxi_simulator

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -3,6 +3,7 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 
 taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
 current_taxi = None
+total_fare = 0
 print("Lets drive:\n q)uit, c)hoose taxi, d)rive")
 choice = input('>>>')
 while choice != 'q'.upper():
@@ -10,13 +11,17 @@ while choice != 'q'.upper():
         print("Taxis available:")
         i = 0
         for taxi in taxis:
-            print(i + print(taxis[i]))
+            print("{}. {}".format(i, (taxis[i])))
             i += 1
-        current_taxi = input("Choose taxi: ")
+        taxi_choice = int(input("Choose taxi: "))
+        current_taxi = taxis[taxi_choice]
+        print("Your taxi is: {}".format(current_taxi))
     elif choice == 'd'.upper():
         distance = int(input("Distance: "))
         current_taxi.drive(distance)
+        print("Your {} trip will cost you {}".format(current_taxi.name, current_taxi.get_fare()))
         total_fare += current_taxi.get_fare()
+        print("Bill to date: ${}".format(total_fare))
     else:
         print("Please enter valid input")
     choice = input('>>>')
