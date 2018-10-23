3b7675aa971ef6d6656b93657f16d707e1aebe4b
Samuel-Hayes <sam.hayes1999@gmail.com>
2018-10-23 11:07:17 +1000

    Sort into folders as categorised

--- prac_09/sort_files.py
+++ prac_09/sort_files.py
@@ -1,7 +1,8 @@
 import os
 import shutil
 
-file_types = [".txt", ".xls", ".xlsx", ".png", ".jpg", ".gif", ".doc", ".docx"]
+file_types = {".txt": '', ".xls": '', ".xlsx": '', ".png": '', ".jpg": '', ".gif": '', ".doc": '', ".docx": ''}
+categories = []
 os.chdir('FilesToSort')
 
 for filename in os.listdir('.'):
@@ -10,3 +11,16 @@ for filename in os.listdir('.'):
             if not os.path.exists(file_type.replace('.', '')):
                 os.mkdir(file_type.replace('.', ''))
             shutil.move(filename, file_type.replace('.', ''))
+
+# for file_type in file_types:
+#     category = str(input("What category would you like to sort {} files into".format(file_type)))
+#     categories.append(category)
+#     file_types[file_type] = category
+#     if not os.path.exists(category):
+#         os.mkdir(category)
+#
+# for filename in os.listdir('.'):
+#     for file_type in file_types:
+#         if filename.endswith(file_type):
+#             shutil.move(filename, file_types[file_type])
+
